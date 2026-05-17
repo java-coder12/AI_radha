a79e3284f0f4370250fd94001101a96ff76ecdd2
java-coder12 <arkfilmers@gmail.com>
2026-05-18 03:07:39 +0530

    Update Selection_Sort.cpp

--- Selection_Sort.cpp
+++ Selection_Sort.cpp
@@ -3,28 +3,40 @@ using namespace std;
 
 int main()
 {
-    int n, arr[10];
+    int n, arr[100];
 
+    cout << "Enter number of elements: ";
     cin >> n;
 
+    cout << "Enter array elements:\n";
+
     for(int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    for(int i = 0; i < n-1; i++)
     {
-        int min = i;
-
-        for(int j = i+1; j < n; j++)
-        {
-            if(arr[j] < arr[min])
-                min = j;
-        }
-
-        swap(arr[i], arr[min]);
+        cin >> arr[i];
     }
 
+    // Selection Sort
+    for(int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+
+        for(int j = i + 1; j < n; j++)
+        {
+            if(arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[i], arr[minIndex]);
+    }
+
+    cout << "Sorted array:\n";
+
     for(int i = 0; i < n; i++)
+    {
         cout << arr[i] << " ";
+    }
 
     return 0;
 }
